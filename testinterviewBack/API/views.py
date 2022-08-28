ba738f9c7d264083fa8af051409cff1932ceae73
Samrolz009 <60324665+Samrolz009@users.noreply.github.com>
2022-08-27 21:28:10 -0600

    need to send back response

--- testinterviewBack/API/views.py
+++ testinterviewBack/API/views.py
@@ -12,18 +12,3 @@ from .models import Problem
 class ProblemViewSet(viewsets.ModelViewSet):
     queryset = Problem.objects.all().order_by('input')
     serializer_class = ProblemSerializer
-
-@api_view(['POST'])
-def getInput(request):
-    serializer  = ProblemSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['PUT'])
-def countNonEmptySubstr(str):
-    n = len(str);
-    return int(n * (n + 1) / 2);
-    
